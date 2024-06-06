81c73caa9699af79b319e76221b52c7ea1d39325
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:23:40 +0000

    fix task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-/*reads and prints the content of a file. */
+// reads and prints the content of a file.
+
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
