ef7d76985e9ec29cca3ee5ac0cc35b21650d6cce
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:52:17 +0000

    task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id.
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
+      }
+    }
+  }
+  console.log(users);
+});
