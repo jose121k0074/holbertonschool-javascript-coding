568e0714eda6016c5de906e7d0d80723154f7ede
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:22:23 +0000

    task 0 readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*reads and prints the content of a file. */
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  process.stdout.write(data);
+});
