7993dba5d12b37b927b0eb12bca55f453b73188a
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:47:10 +0000

    task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints the number of movies where the character "Wedge Antilles" is present.
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  let con = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      if (character.endsWith('/18/')) {
+        con++;
+      }
+    }
+  }
+  console.log(con);
+});
