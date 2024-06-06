2567beba1a834e6881e40e3b59510dd4d21f28fb
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:43:16 +0000

    fix bug task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-//prints the title of a Star Wars movie where the episode
-
+// prints the title of a Star Wars movie where the episode
 const request = require('request');
-const link = 'http://swapi-api.hbtn.io/api/films/';
+const link = 'https://swapi-api.hbtn.io/api/films/';
+const url = link + parseInt(process.argv[2]);
 
-const url = link  + parseInt(process.argv[2]);
-request(url, function (error, response, body) {
-  if (error) throw error;
-  console.log(JSON.parse(body).title);
+request({ url: `${url}`, json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching the URL:', error);
+    return;
+  }
+  console.log(body.title);
 });
