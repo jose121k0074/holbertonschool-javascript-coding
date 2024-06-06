eeaaf14d325265d916470c1b237557ddee7826be
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-06 22:39:31 +0000

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,9 @@
 //prints the title of a Star Wars movie where the episode
 
 const request = require('request');
+const link = 'http://swapi-api.hbtn.io/api/films/';
 
-const url = 'http://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
+const url = link  + parseInt(process.argv[2]);
 request(url, function (error, response, body) {
   if (error) throw error;
   console.log(JSON.parse(body).title);
